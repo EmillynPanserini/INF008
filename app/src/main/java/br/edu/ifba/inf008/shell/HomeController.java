60648aa8764cb71fa43e5442eb8434e0f9f54483
EmillynPanserini <emillyn2003@gmail.com>
2025-08-01 11:51:14 -0300

    Ajustes na tela home e user

---
+++ app/src/main/java/br/edu/ifba/inf008/shell/HomeController.java
@@ -0,0 +1,49 @@
+package br.edu.ifba.inf008.shell;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class HomeController {
+
+    @FXML
+    void handleMenu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleMenuBooks(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleMenuExit(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleMenuHome(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleMenuLoan(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleMenuLoanReports(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleMenuProfile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleMenuUsers(MouseEvent event) {
+
+    }
+
+}
